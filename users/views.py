cb9df6dcba37da1a86c0fb544337670484381c4b
Shelton Gayles <sgbythelake@gmail.com>
2019-03-02 18:33:15 -0800

    created front end user login and logout forms

--- users/views.py
+++ users/views.py
@@ -10,7 +10,9 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
+            messages.success(
+                request, f"Your account has be created! You are able to login"
+            )
             return redirect("blog-home")
     else:
         form = UserRegisterForm()
