d806a8ac9ceb0e4c49352595344e1b0e4fb832ad
Shelton Gayles <sgbythelake@gmail.com>
2019-03-03 12:32:23 -0800

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = "blog/home.html"
     context_object_name = "posts"  # which I am already using
     ordering = ["-date_posted"]  # sets the order by in revers (-)
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
