c67ccc67676f19242195ebe26df1e873f6e543a7
Shelton Gayles <sgbythelake@gmail.com>
2019-03-03 12:50:50 -0800

    created a view by author

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,6 +8,7 @@ from django.views.generic import (
 )
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -23,6 +24,19 @@ class PostListView(ListView):
     context_object_name = "posts"  # which I am already using
     ordering = ["-date_posted"]  # sets the order by in revers (-)
     paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    # Default view is something like <app>/<model>_<viewtype>.html
+    # But can change to existing template
+    template_name = "blog/user_posts.html"
+    context_object_name = "posts"  # which I am already using
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
 
 
 class PostDetailView(DetailView):
